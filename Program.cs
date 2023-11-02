60df090023a7d994f07f18837b7cba39e0c05355
Hani <Hani@DESKTOP-Q9D08B7>
2023-11-02 14:35:46 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DONMBALE_MOSLASI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" number1: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("number2: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            for (double i = num1; i <= num2; i++)
+            {
+                double Donbale1 = (i*(i+1));
+                double Donbale2 = Donbale1/2;
+                
+                
+
+
+
+
+                Console.WriteLine("Donbale: {0}", Donbale2);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
